78c4f2fdc15656dd5bec607f1694cda5d2524856
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-29 17:32:34 -0400

    commit tree 146fe6ddf2f80da343c0092b12c21f8eb615e48e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    //returns the number of blocks given a value
+    return Math.abs(42 - pickupLocation);
+  }
+
+
+function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue);
+      
+}
+
+
+  function distanceTravelledInFeet(feet1, feet2) {
+    //returns the number of feet traveled
+      return Math.abs(feet1 - feet2) * 264;
+  }
+
+  function calculatesFarePrice(feet1, feet2) {
+    //returns the fare for the customer
+      let distanceInFeet = distanceTravelledInFeet(feet1, feet2);
+      let fare;
+      if (distanceInFeet >= 400 && distanceInFeet >= 2500)
+      {
+          fare = 25;
+          return fare;
+      }
+      else if (distanceInFeet > 2500)
+      {
+          fare = "cannot travel that far";
+      }
+      return fare;
+  }
